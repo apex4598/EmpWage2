725cea71118c44ecae245b8067dbd3aac3d63107
Apex4598 <login2ajit@gmail.com>
2022-06-05 18:06:52 +0530

    Save the Toal Wage For Each Company

--- src/com/Bridgelabz/SaveTotalWages.java
+++ src/com/Bridgelabz/SaveTotalWages.java
@@ -1,6 +1,6 @@
 package com.Bridgelabz;
 
-public class ComputeMultipleComp {
+public class SaveTotalWages {
     public final static int IS_PART_TIME = 1;
     public final static int IS_FULL_TIME = 2;
 
@@ -10,21 +10,14 @@ public class ComputeMultipleComp {
     int Max_Hrs_In_Month;
 
 
-    public  ComputeMultipleComp(String company,int Emp_Rate_Per_Hr, int Num_Of_Working_Days,int Max_Hrs_In_Month)
+    public int  evaluateEmpWage(String company,int Emp_Rate_Per_Hr, int Num_Of_Working_Days,int Max_Hrs_In_Month)
 
-    {
-        this.company=company;
-        this.Emp_Rate_Per_Hr=Emp_Rate_Per_Hr;
-        this.Num_Of_Working_Days=Num_Of_Working_Days;
-        this.Max_Hrs_In_Month=Max_Hrs_In_Month;
-    }
-
-    public void empcal()
     {
         int empHrs = 0 ;
         int totalEmpHrs = 0 ;
         int totalWorkingDays = 0;
         int totalEmpWage=0;
+
 
         while (totalEmpHrs <= Max_Hrs_In_Month &&
                 totalWorkingDays < Num_Of_Working_Days)
@@ -45,18 +38,24 @@ public class ComputeMultipleComp {
             System.out.println("Day: " + totalWorkingDays +"  "+ "Employee working hours:" + empHrs );
         }
         totalEmpWage = totalEmpHrs * Emp_Rate_Per_Hr;
-        System.out.println("Total Employee Wage is "+ totalEmpWage);
+        return totalEmpWage;
     }
 
     public static void main(String args[])
     {
-        ComputeMultipleComp obj1 =new ComputeMultipleComp("Flipkart",14,15,66);
-        ComputeMultipleComp obj2 =new ComputeMultipleComp("Amazon",12,14,55);
-        System.out.println("Empolyee Wage For the " +obj1.company);
-        obj1.empcal();
-        System.out.println("----------------------------------------");
-        System.out.println("Empolyee Wage For the " +obj2.company);
-        obj2.empcal();
+        SaveTotalWages emp=new SaveTotalWages();
+        SaveTotalWages obj1  = new SaveTotalWages();
+
+        int[] wages=new int[3];
+
+        wages[0]=emp.evaluateEmpWage("Flipkart",40,20,100);
+        System.out.println("Employee Monthly wage"+wages[0]+"in Flipkart");
+        System.out.println("---------------------------------------------");
+        wages[1]=emp.evaluateEmpWage("Amazon",50,20,120);
+        System.out.println("Employee Monthly wage"+wages[1]+"in Amazon");
+        System.out.println("---------------------------------------------");
+        wages[2]=emp.evaluateEmpWage("Mesho",50,18,20);
+        System.out.println("Employee Monthly wage"+wages[2]+"in Mesho");
+        System.out.println("---------------------------------------------");
     }
 }
-
